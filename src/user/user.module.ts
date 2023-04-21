981be7559b267ecd6a32ecd3303009665c67bda2
ayoob <ayoob.khodadadi@gmail.com>
2023-04-21 12:56:06 +0330

    feat(users): add user repository

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user/user';
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forFeature([User])],
     providers: [],
     controllers: []
 })
