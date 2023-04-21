b6ca6de993afe33070f73df6f3f26919fba54ad8
ayoob <ayoob.khodadadi@gmail.com>
2023-04-21 15:52:53 +0330

    feat(users): add user controller

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user/user';
 import { UserService } from './services/user/user.service';
 import { UserMapperService } from './services/user-mapper/user-mapper.service';
+import { UserController } from './controllers/user/user.controller';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([User])],
     providers: [UserService, UserMapperService],
-    controllers: []
+    controllers: [UserController]
 })
 export class UserModule { }
